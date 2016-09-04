bb9c4c306532682f562c8bef8f37e556cf31daa8
sporter <sporter@ownapt.com>
2016-09-03 23:43:09 -0600

    22 : Created

---
+++ src/ApiContract/Dto/AmmenityDto.cs
@@ -0,0 +1,26 @@
+﻿using OwnApt.Common.Dto;
+using OwnApt.Common.Enum;
+using OwnApt.Common.Extension;
+
+namespace OwnApt.Api.Contract.Dto
+{
+    public class AmmenityDto : Equatable<AmmenityDto>
+    {
+        #region Public Properties
+
+        public string Description { get; set; }
+        public AmmenityType Type { get; set; }
+
+        #endregion Public Properties
+
+        #region Public Methods
+
+        public override int GetHashCode()
+        {
+            return this.Description.GetHashCodeSafe()
+                       ^ this.Type.GetHashCodeSafe();
+        }
+
+        #endregion Public Methods
+    }
+}
---
+++ src/ApiContract/Dto/ContactDto.cs
@@ -0,0 +1,25 @@
+﻿using OwnApt.Common.Dto;
+using OwnApt.Common.Extension;
+
+namespace OwnApt.Api.Contract.Dto
+{
+    public class ContactDto : Equatable<ContactDto>
+    {
+        #region Public Properties
+
+        public string Email { get; set; }
+        public PhoneDto[] Phones { get; set; }
+
+        #endregion Public Properties
+
+        #region Public Methods
+
+        public override int GetHashCode()
+        {
+            return this.Email.GetHashCodeSafe()
+                 ^ this.Phones.GetHashCodeSafe();
+        }
+
+        #endregion Public Methods
+    }
+}
