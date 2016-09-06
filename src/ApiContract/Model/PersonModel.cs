d940af2336b898ba03e30e55d0db0f21223ced99
sporter <sporter@ownapt.com>
2016-09-05 21:17:03 -0600

    Revert "22 : Switched concrete's to interfaces so both client and API can implement their own models/dtos"
    This reverts commit 568d20557bfe9c809de88d933d92128c4280fd7e.

---
+++ src/ApiContract/Model/PersonModel.cs
@@ -0,0 +1,39 @@
+﻿using OwnApt.Api.Contract.Dto;
+using OwnApt.Common.Dto;
+using OwnApt.Common.Enum;
+using OwnApt.Common.Extension;
+
+namespace OwnApt.Api.Contract.Model
+{
+    public class PersonModel : Equatable<PersonModel>
+    {
+        #region Public Properties
+
+        public int Age { get; set; }
+        public ContactDto Contact { get; set; }
+        public int CreditScore { get; set; }
+        public Gender Gender { get; set; }
+        public string Id { get; set; }
+        public NameDto Name { get; set; }
+        public string[] PropertyIds { get; set; }
+        public PersonType Type { get; set; }
+
+        #endregion Public Properties
+
+        #region Public Methods
+
+        public override int GetHashCode()
+        {
+            return this.Id.GetHashCodeSafe()
+                ^ this.PropertyIds.GetHashCodeSafe()
+                ^ this.Type.GetHashCodeSafe()
+                ^ this.Name.GetHashCodeSafe()
+                ^ this.Age.GetHashCodeSafe()
+                ^ this.Gender.GetHashCodeSafe()
+                ^ this.CreditScore.GetHashCodeSafe()
+                ^ this.Contact.GetHashCodeSafe();
+        }
+
+        #endregion Public Methods
+    }
+}
