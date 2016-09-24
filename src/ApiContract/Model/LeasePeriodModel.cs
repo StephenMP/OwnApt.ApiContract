e3f4b9ace7f6ea965c9bbf1a849da2cba79deaf9
sporter <sporter@ownapt.com>
2016-09-23 22:07:23 -0600

    Added lease period model

---
+++ src/ApiContract/Model/LeasePeriodModel.cs
@@ -0,0 +1,16 @@
+﻿using OwnApt.Common.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OwnApt.Api.Contract.Model
+{
+    public class LeasePeriodModel
+    {
+        public int LeasePeriodId { get; set; }
+        public int LeaseTermId { get; set; }
+        public LeasePeriodStatus LeasePeriodStatus { get; set; }
+        public string PeriodId { get; set; }
+    }
+}
--- src/ApiContract/Model/LeaseTermModel.cs
+++ src/ApiContract/Model/LeaseTermModel.cs
@@ -1,5 +1,6 @@
 ﻿using OwnApt.Common.Dto;
 using System;
+using System.Collections.Generic;
 
 namespace OwnApt.Api.Contract.Model
 {
@@ -12,6 +13,8 @@ namespace OwnApt.Api.Contract.Model
         public string PropertyId { get; set; }
         public decimal Rent { get; set; }
         public DateTime StartDate { get; set; }
+        public bool Active { get; set; }
+        public List<LeasePeriodModel> LeasePeriods { get; set; }
 
         #endregion Public Properties
     }
