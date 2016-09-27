c1edeb65a5e0fedd78d7dc32ec1bf71c2c1c9a15
sporter <sporter@ownapt.com>
2016-09-26 20:09:00 -0600

    #51 : Ensuring one time use tokens

---
+++ src/ApiContract/Model/RegisteredTokenModel.cs
@@ -0,0 +1,14 @@
+﻿using OwnApt.Common.Dto;
+
+namespace OwnApt.Api.Contract.Model
+{
+    public class RegisteredTokenModel : Equatable
+    {
+        #region Public Properties
+
+        public string Token { get; set; }
+        public int TokenId { get; set; }
+
+        #endregion Public Properties
+    }
+}
