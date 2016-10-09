23edf251c979785207c73046abfe3c061c61867e
sporter <sporter@ownapt.com>
2016-10-08 20:00:15 -0600

    #56 : Adding domain model

---
+++ src/ApiContract/Model/FeatureToggleModel.cs
@@ -0,0 +1,13 @@
+﻿namespace OwnApt.Api.Contract.Model
+{
+    public class FeatureToggleModel
+    {
+        #region Public Properties
+
+        public bool Enabled { get; set; }
+        public string Id { get; set; }
+        public string Name { get; set; }
+
+        #endregion Public Properties
+    }
+}
