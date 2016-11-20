8bcc5c8893fab1d345470de3ead5a3150ca9912b
sporter <sporter@ownapt.com>
2016-11-19 20:43:28 -0700

    Adding model to support management agreements

---
+++ src/ApiContract/Model/ManagementAgreementModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OwnApt.Common.Dto;
+
+namespace OwnApt.Api.Contract.Model
+{
+    public class ManagementAgreementModel : Equatable
+    {
+        public string Id { get; set; }
+        public int Version { get; set; }
+        public List<ManagementAgreementSectionModel> Sections { get; set; }
+
+    }
+
+    public class ManagementAgreementSectionModel : Equatable
+    {
+        public string Title { get; set; }
+        public string Body { get; set; }
+    }
+}
